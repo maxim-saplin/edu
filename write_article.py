cc14ae4aa1577c53b0d5850560b31c9332617384
Maxim Saplin <smaxmail@gmail.com>
2023-10-17 15:20:19 +0300

    More

--- write_article.py
+++ write_article.py
@@ -27,19 +27,19 @@ user_proxy = autogen.UserProxyAgent(
     name="smax",
     human_input_mode="TERMINATE",
     max_consecutive_auto_reply=15,
-    is_termination_msg=lambda x: x.get ("content", "").rstrip().endswith("TERMINATE"),
+    #is_termination_msg=lambda x: x.get ("content", "").rstrip().endswith("TERMINATE"),
     code_execution_config={"work_dir":"_output2", "use_docker":"python:3"},
     llm_config=llm_config,
-    system_message=""""
-    You are a professional article reviwer and software engineer. Carefully evaluate the taks and you peer answers. 
-    Reply TERMINATE if the task has been solved at full satisfaction. Otherwise, reply CONTINUE, or the reason why the task is not solved.
-    """
+    # system_message=""""
+    # You are a professional article reviwer and software engineer. Carefully evaluate the taks and you peer answers. 
+    # Reply TERMINATE if the task has been solved at full satisfaction. Otherwise, reply CONTINUE, or the reason why the task is not solved.
+    # """
 )
 
 task = """
 Your task is to write an article about AutoGen. The idea is to give a quick start guide.
-First, you need yo evaluate the docs about AutoGen at https://microsoft.github.io/autogen/, make sure to follow links at the web site and gather deep enough understanding of the library.
-Second, you will be describing a sample project located at: https://github.com/maxim-saplin/autogen_sample/blob/main/write_article.py
+First,evaluate the docs about AutoGen at https://microsoft.github.io/autogen/. Make sure to follow links at the web site and gather deep enough understanding of the library. Please create a web crawl plan, present the pages and write down parts that might be relevant to our task.
+Second, describe sample project located at: https://github.com/maxim-saplin/autogen_sample/blob/main/write_article.py
 Your result must be a markdown file named 'article.md' that will be used to publish it at a public blogging platform.
 
 Article acceptance criteria:
